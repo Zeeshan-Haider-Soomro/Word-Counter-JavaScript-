1e8d20c9dcb2269156114c0ef502e4162574151b
Zeeshan-Haider-Soomro <159009736+Zeeshan-Haider-Soomro@users.noreply.github.com>
2024-05-17 16:05:50 +0500

    word counter application

---
+++ app.js
@@ -0,0 +1,18 @@
+function counterApplication(){
+    let paragraph = document.querySelector("textarea")
+    
+    let words = document.querySelector("#words")
+    let sentences = document.querySelector("#sentences")
+    let character = document.querySelector("#character")
+    let para = document.querySelector("#para")
+
+    let characters = paragraph.value.length
+    let word = paragraph.value.split(" ").length;
+    let sentence = paragraph.value.split(".").length
+    let paras = paragraph.value.split("\n").length
+
+    words.innerText = ` Total Words ${word} `
+    sentences.innerText = ` Total Sentences ${sentence} `
+    character.innerText = ` Total Characters ${characters} `
+    para.innerText = ` Total Paragraphs ${paras} `
+}
